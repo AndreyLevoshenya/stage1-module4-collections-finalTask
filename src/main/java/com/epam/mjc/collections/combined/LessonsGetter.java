04f005a2c3869c7142705a882c887f0b3da712ca
Andrey <98894778+AndreyLevoshenya@users.noreply.github.com>
2023-04-13 15:36:06 +0300

    Add files via upload

--- src/main/java/com/epam/mjc/collections/combined/LessonsGetter.java
+++ src/main/java/com/epam/mjc/collections/combined/LessonsGetter.java
@@ -1,10 +1,16 @@
 package com.epam.mjc.collections.combined;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 public class LessonsGetter {
     public Set<String> getLessons(Map<String, List<String>> timetable) {
+        Set<String> classes = new HashSet<>();
+        for (String key : timetable.keySet()) {
+            classes.addAll(timetable.get(key));
+        }
+        return classes;
     }
 }
--- src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
+++ src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
@@ -1,9 +1,27 @@
 package com.epam.mjc.collections.combined;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
 public class MapFromKeysCreator {
     public Map<Integer, Set<String>> createMap(Map<String, Integer> sourceMap) {
+        Map<Integer, Set<String>> wordLenMap = new HashMap<>();
+        for (Map.Entry<String, Integer> entry : sourceMap.entrySet()) {
+            String word = entry.getKey();
+            wordLenMap.put(word.length(), new HashSet<>());
+        }
+        for (Map.Entry<Integer, Set<String>> entryWordLenMap : wordLenMap.entrySet()) {
+            for (Map.Entry<String, Integer> entrySourceMap : sourceMap.entrySet()) {
+                String word = entrySourceMap.getKey();
+                if(word.length() == entryWordLenMap.getKey()) {
+                    Set<String> words = entryWordLenMap.getValue();
+                    words.add(word);
+                    entryWordLenMap.setValue(words);
+                }
+            }
+        }
+        return wordLenMap;
     }
 }
