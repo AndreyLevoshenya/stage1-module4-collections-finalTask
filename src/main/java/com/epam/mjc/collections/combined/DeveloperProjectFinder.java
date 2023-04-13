aa3f67056848eb593c039f7acd48d6714bd625cb
Andrey <98894778+AndreyLevoshenya@users.noreply.github.com>
2023-04-13 15:42:42 +0300

    Add files via upload

--- src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
+++ src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
@@ -6,10 +6,11 @@ public class DeveloperProjectFinder {
     public List<String> findDeveloperProject(Map<String, Set<String>> projects, String developer) {
         List<String> listOfProjects = new LinkedList<>();
         for (String project : projects.keySet()) {
-            if(projects.get(project).contains(developer)) {
+            if (projects.get(project).contains(developer)) {
                 listOfProjects.add(project);
             }
         }
+        listOfProjects.sort((o1, o2) -> o2.length() - o1.length() == 0 ? o2.compareTo(o1) : o2.length() - o1.length());
         return listOfProjects;
     }
 }
